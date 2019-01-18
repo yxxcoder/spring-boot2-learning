300bbf848e60324b04241161e56d24a8806aec7e
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-01-19 00:00:40 +0800

    构建一个SpringBoot项目

---
+++ chapter1/src/main/java/com/simple/chapter1/Chapter1Application.java
@@ -0,0 +1,13 @@
+package com.simple.chapter1;
+
+/**
+ * 构建第一个Spring Boot工程
+ *
+ * @author yxxcoder
+ * @create 2019-01-18 11:58 PM
+ **/
+public class Chapter1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter1Application.class, args);
+    }
+}
