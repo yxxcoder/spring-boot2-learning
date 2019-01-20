6bf0e938c916842f42a2398b70c842d45c5675f1
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-01-20 22:27:40 +0800

    Add Test

---
+++ chapter1/src/test/java/com/simple/chapter1/Chapter1ApplicationTest.java
@@ -0,0 +1,40 @@
+package com.simple.chapter1;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.net.URL;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Chapter1ApplicationTest {
+
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        // 因为修改了 content-path 所以请求后面要带上
+        this.base = new URL("http://localhost:" + port + "/simple/demo1");
+    }
+
+    @Test
+    public void demo1() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertEquals(response.getBody(), "Hello");
+    }
+}
