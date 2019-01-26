d90957e30aa956e5fd4aea2f0a766f11fa2e54d8
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-01-26 23:53:08 +0800

    Logback Setting

---
+++ chapter3/src/main/java/com/simple/chapter3/Chapter3Application.java
@@ -0,0 +1,17 @@
+package com.simple.chapter3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot日志配置
+ *
+ * @author yxxcoder
+ * @create 2019-01-26 11:43 PM
+ **/
+@SpringBootApplication
+public class Chapter3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3Application.class, args);
+    }
+}
