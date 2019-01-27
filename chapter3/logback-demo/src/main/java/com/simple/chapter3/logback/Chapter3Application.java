9113938c0e56dc3858c7be22b62bdd30d5d5b3ce
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-01-27 23:51:28 +0800

    Add Module

--- chapter3/logback-demo/src/main/java/com/simple/chapter3/logback/Chapter3Application.java
+++ chapter3/logback-demo/src/main/java/com/simple/chapter3/logback/Chapter3Application.java
@@ -1,4 +1,4 @@
-package com.simple.chapter3;
+package com.simple.chapter3.logback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
