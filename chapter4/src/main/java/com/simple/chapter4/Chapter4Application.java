5fa4a7ff39550b3c098605a9631f17effb1237c3
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-01-28 23:56:46 +0800

    Thymeleaf Demo

---
+++ chapter4/src/main/java/com/simple/chapter4/Chapter4Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 整合Thymeleaf模板
+ *
+ * @author yxxcoder
+ * @create 2019-01-22 11:42 PM
+ **/
+@SpringBootApplication
+public class Chapter4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter4Application.class, args);
+    }
+
+}
