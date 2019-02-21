4a6d9804d37406f84008cfc9ab5fa9c84ee93780
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-02-21 21:15:01 +0800

    JdbcTemplate Demo Project

---
+++ chapter5/src/main/java/com/simple/chapter5/Chapter5Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 使用JdbcTemplate访问数据库
+ *
+ * @author yxxcoder
+ * @create 2019-02-21 11:42 PM
+ **/
+@SpringBootApplication
+public class Chapter5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter5Application.class, args);
+    }
+
+}
