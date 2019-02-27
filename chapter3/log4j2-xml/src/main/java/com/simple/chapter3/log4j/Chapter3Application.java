ee3c70227c5cf8ba4a22cbe80605f885ef5163de
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-02-27 21:35:26 +0800

    log4j2 properties and xml setting files

---
+++ chapter3/log4j2-xml/src/main/java/com/simple/chapter3/log4j/Chapter3Application.java
@@ -0,0 +1,25 @@
+package com.simple.chapter3.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot Log4j2日志配置
+ *
+ * @author yxxcoder
+ * @create 2019-01-26 11:43 PM
+ **/
+@SpringBootApplication
+public class Chapter3Application {
+
+    private static Logger logger = LoggerFactory.getLogger(Chapter3Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3Application.class, args);
+        logger.debug("Start Success..");
+        logger.info("Logger INFO..");
+        logger.error("Logger ERROR..");
+    }
+}
