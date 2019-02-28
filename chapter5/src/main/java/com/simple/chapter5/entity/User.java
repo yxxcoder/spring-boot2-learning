40b8c2c169cce09372a874757fe9b620fca5dfcd
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-02-28 21:01:46 +0800

    JdbcTemplate Demo and Test

---
+++ chapter5/src/main/java/com/simple/chapter5/entity/User.java
@@ -0,0 +1,61 @@
+package com.simple.chapter5.entity;
+
+/**
+ * @author yxxcoder
+ * @create 2019-02-28 8:34 PM
+ **/
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
+
