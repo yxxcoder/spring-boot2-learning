4fdc35418eb00d476db86467b7f8fd0402304960
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-03-05 23:38:03 +0800

    Mybatis PageHelper Dependencies

---
+++ chapter8/src/main/java/com/simple/chapter8/Chapter8Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 通用Mapper与分页插件的集成
+ *
+ * @author yxxcoder
+ * @create 2019-03-03 11:42 PM
+ **/
+@SpringBootApplication
+public class Chapter8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter8Application.class, args);
+    }
+
+}
