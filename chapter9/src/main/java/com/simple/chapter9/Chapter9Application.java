3afe0679a7161dacbf25a41ae68be8df6f5d902e
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-03-08 17:30:48 +0800

    Add Module

---
+++ chapter9/src/main/java/com/simple/chapter9/Chapter9Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 整合Lettuce Redis
+ *
+ * @author yxxcoder
+ * @create 2019-03-08 17:29 PM
+ **/
+@SpringBootApplication
+public class Chapter9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter9Application.class, args);
+    }
+
+}
