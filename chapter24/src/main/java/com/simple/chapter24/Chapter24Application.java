c2ede274786bf73341a83435cb2d8265bfc68f8f
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-03-28 23:59:20 +0800

    Liquibase

---
+++ chapter24/src/main/java/com/simple/chapter24/Chapter24Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 数据库管理与迁移（Liquibase）
+ *
+ * @author yxxcoder
+ * @since 2019-03-28 23:50 PM
+ */
+@SpringBootApplication
+public class Chapter24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter24Application.class, args);
+    }
+}
