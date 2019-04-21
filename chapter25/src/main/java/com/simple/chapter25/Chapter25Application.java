39cef744273b5cdbb8511d7ca7926baf470553b9
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-04-21 23:18:53 +0800

    WebSocket

---
+++ chapter25/src/main/java/com/simple/chapter25/Chapter25Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 打造属于你的聊天室（WebSocket）
+ *
+ * @author yxxcoder
+ * @since 2019-03-30 23:00 PM
+ */
+@SpringBootApplication
+public class Chapter25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter25Application.class, args);
+    }
+}
