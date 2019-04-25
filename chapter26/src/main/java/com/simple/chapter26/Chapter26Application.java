dd55f1f2a5ad9927d39972264b674dba31740806
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-04-25 22:27:50 +0800

    init project

---
+++ chapter26/src/main/java/com/simple/chapter26/Chapter26Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 轻松搞定安全框架（Shiro）
+ *
+ * @author yxxcoder
+ * @since 2019-04-25 22:25 PM
+ */
+@SpringBootApplication
+public class Chapter26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter26Application.class, args);
+    }
+
+}
