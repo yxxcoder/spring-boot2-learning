09e558ebf037fd2888a3fa81a0e4dcfbfc1536b5
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-06-09 20:20:29 +0800

    add dependencies

---
+++ chapter27/src/main/java/com/simple/chapter27/Chapter27Application.java
@@ -0,0 +1,19 @@
+package com.simple.chapter27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 优雅解决分布式限流
+ *
+ * @author yxxcoder
+ * @since 2019-06-09 20:15 PM
+ */
+@SpringBootApplication
+public class Chapter27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter27Application.class, args);
+    }
+
+}
